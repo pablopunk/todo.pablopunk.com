7d95393c0459235e20d9ebf3e2cc79f35e729deb
pablopunk <yo@pablo.pink>
2020-06-09 13:47:25 +0200

    Allow pagination (fix #1)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const fetch = require('node-fetch')
 
 module.exports = async (req, res) => {
-  let [_0, org] = req.url.split('/')
-  org = org.split('?')[0]
+  const [_0, pathname] = req.url.split('/')
+  const [org, params] = pathname.split('?')
+
+  // if url is /search, then org=search will beignored
+  // cause params should also contain a ?org=value
 
   const githubRes = await fetch(
-    `https://github.com/search?q=org%3A${org}+is%3Aopen`
+    `https://github.com/search?q=org%3A${org}+is%3Aopen&${params}`
   )
   const html = (await githubRes.text()).replace(
     '</head>',
